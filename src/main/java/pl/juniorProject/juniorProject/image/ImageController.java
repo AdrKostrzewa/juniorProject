715fd3b821637d8458ff72f88765587394e06190
dom <marsonowski14@gmail.com>
2020-05-20 16:46:50 +0200

    -addition cloudinary (Service, Controller, Config) application saves files to cloudinary

---
+++ src/main/java/pl/juniorProject/juniorProject/image/ImageController.java
@@ -0,0 +1,27 @@
+package pl.juniorProject.juniorProject.image.facade;
+
+import io.swagger.annotations.Api;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import pl.juniorProject.juniorProject.image.ImageService;
+
+@RestController
+@Api
+@RequestMapping("/images")
+public class ImageController {
+    private final ImageService imageService;
+
+    @Autowired
+    public ImageController(ImageService imageService) {
+        this.imageService = imageService;
+    }
+
+    @PostMapping
+    public void addBook(@RequestBody MultipartFile file){
+        this.imageService.addBook(file);
+    }
+}
